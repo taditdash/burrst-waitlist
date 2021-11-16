1183c975a6a6c8b2020f4744c64212a382d81a56
Tadit Dash <tadit.dash@rapidfunnel.com>
2021-11-16 20:35:54 +0530

    New design.

--- assets/scripts/js/myScript.js
+++ assets/scripts/js/myScript.js
@@ -9,9 +9,10 @@ $(document).ready(function() {
 		//email[0], 
 		var input = document.querySelector("form input");
 		changeValue(input, $('#txtEmail').val())
-		
+		document.getElementById('txtEmail').value = ' ';
 		//$('.input--waitlistapi').val($('#txtEmail').val());
 		$('.button--waitlistapi').click();
+		
 	});
 });
 
@@ -26,3 +27,15 @@ function changeValue(input,value){
     var inputEvent = new Event("input", { bubbles: true });
     input.dispatchEvent(inputEvent);
 }
+
+
+$("#typed").typed({
+	strings: ["Every Sport You Can Dream Of",],
+	typeSpeed: 100,
+	startDelay: 0,
+	backSpeed: 60,
+	backDelay: 1000,
+	loop: true,
+	cursorChar: "|",
+	contentType: 'html'
+});
